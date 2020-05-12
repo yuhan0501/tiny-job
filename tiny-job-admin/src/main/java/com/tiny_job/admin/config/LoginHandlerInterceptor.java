201edd41fa1d6abd2801dec187d2d862c2923fe3
yuhan <yu.han2@iwhalecloud.com>
2020-05-12 12:46:29 +0800

    add static

---
+++ tiny-job-admin/src/main/java/com/tiny_job/admin/config/LoginHandlerInterceptor.java
@@ -0,0 +1,11 @@
+package com.tiny_job.admin.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * @description:
+ * @author: yuhan
+ * @create: 2020-05-11 15:45
+ **/
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+}
