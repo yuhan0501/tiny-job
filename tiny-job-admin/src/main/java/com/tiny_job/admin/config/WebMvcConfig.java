83cae62ac732ff83ceba708efc22723550c159fb
yuhan <yu.han2@iwhalecloud.com>
2020-05-12 12:46:29 +0800

    add static

---
+++ tiny-job-admin/src/main/java/com/tiny_job/admin/config/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.tiny_job.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @description:
+ * @author: yuhan
+ * @create: 2020-05-11 15:41
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    /**
+     * 添加静态资源文件，外部可以直接访问地址
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .excludePathPatterns("/**/*.*");
+    }
+
+}
