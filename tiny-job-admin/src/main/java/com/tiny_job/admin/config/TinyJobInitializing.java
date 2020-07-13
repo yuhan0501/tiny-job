4778bc44a633fabf0ea1547b34f2834a637a6919
yuhan <yu.han2@iwhalecloud.com>
2020-07-13 16:23:52 +0800

    1.add docker plugin 2.change initializing mode

---
+++ tiny-job-admin/src/main/java/com/tiny_job/admin/config/TinyJobInitializing.java
@@ -0,0 +1,27 @@
+package com.tiny_job.admin.config;
+
+import com.tiny_job.admin.core.thread.JobScheduleHelper;
+import com.tiny_job.admin.core.thread.JobTriggerPoolHelper;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description:
+ * @author: yuhan
+ * @create: 2020-05-20 17:13
+ **/
+@Configuration
+public class TinyJobInitializing implements InitializingBean {
+
+    @Autowired
+    private JobTriggerPoolHelper jobTriggerPoolHelper;
+    @Autowired
+    private JobScheduleHelper jobScheduleHelper;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        jobTriggerPoolHelper.init();
+        jobScheduleHelper.start();
+    }
+}
