09b1e8e002fe22c3e43ed78092a5fb8e265838f5
Ferris <bin@edmodo.com>
2020-07-13 22:22:16 +0800

    chore:配置接口域名及地址

--- tiny-job-admin-web/src/config.js
+++ tiny-job-admin-web/src/config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
 
   api: {  // 对后端请求的相关配置
-    host: 'http://localhost:8082',  // 调用ajax接口的地址, 默认值空, 如果是跨域的, 服务端要支持CORS
+    host: 'http://tiny-job.c85eaf0d05d04465a81befded3f4f608b.cn-shenzhen.alicontainer.com',  // 调用ajax接口的地址, 默认值空, 如果是跨域的, 服务端要支持CORS
     path: '/tiny-job',  // ajax请求的路径
     timeout: 15000,  // 请求的超时时间, 单位毫秒
   },
